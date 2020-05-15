23c142f4cc98cf94c2737c29c7f1563bdd0657d7
mauricioolarte <reneolarte@yahoo.com>
2020-05-15 00:41:26 +0000

    add

---
+++ add_stack.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "monty.h"
+
+void add_stack(stack_t **head, int line_number)
+{
+
+
+	if (head == NULL || (*head)->next == NULL)
+	{
+		fprintf(stderr, "L%i: can't add, stack too short", line_number);
+			exit(EXIT_FAILURE);
+	}
+
+	(*head)->next->n = (*head)->next->n + (*head)->n;
+}
