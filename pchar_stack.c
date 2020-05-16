0603215310c17f134c38dc0a8ebb6a9952a59cdd
mauricioolarte <reneolarte@yahoo.com>
2020-05-16 21:52:56 +0000

    pchar

--- pchar_stack.c
+++ pchar_stack.c
@@ -6,18 +6,20 @@ int pchar_stack(stack_t **head, int line_number)
 {
 	int character = 0;
 
-	character = (*head)->n;
-	if ((*head) == NULL)
+
+	if ((*head) == NULL )
 	{
 		fprintf(stderr, "L%i: can't pchar, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	else if (character < 0 && character > 255)
+	character = (*head)->n;
+	if (character < 0 || character > 127)
 	{
 		fprintf(stderr, "L%i: can't pchar, value out of range\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	else
+
+
 		printf("%c\n", character);
 	return 1;
 }
