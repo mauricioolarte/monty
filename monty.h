f105a3e9fb5ca104e3d608dcbfb94f72bbd6307e
mauricioolarte <reneolarte@yahoo.com>
2020-05-16 19:59:21 +0000

    monty.h

--- monty.h
+++ monty.h
@@ -38,7 +38,8 @@ size_t print_stack(const stack_t *h);
 int is_integer(char *str);
 size_t print_pint(const stack_t *h, int line_number);
 void free_stack(stack_t *head);
-stack_t *delete_dnodeint_at_index(stack_t **head, unsigned int index, int line_number);
+stack_t *delete_dnodeint_at_index(stack_t **head, unsigned int index,
+				  int line_number);
 void add_stack(stack_t **head, int line_number);
 stack_t *swap_stack(stack_t **head, int line_number);
 void sub_stack(stack_t **head, int line_number);
