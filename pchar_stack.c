7d9e7a2c66dc0942f28966dd36f24cb25efe7972
mauricioolarte <reneolarte@yahoo.com>
2020-05-16 20:59:55 +0000

    PCHAR

--- pchar_stack.c
+++ pchar_stack.c
@@ -9,12 +9,12 @@ int pchar_stack(stack_t **head, int line_number)
 	character = (*head)->n;
 	if ((*head) == NULL)
 	{
-		fprintf(stderr, "L%i: can't pchar, stack empty", line_number);
+		fprintf(stderr, "L%i: can't pchar, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	else if (!(character > 64 && character < 90) || !(character > 96 && character < 123))
 	{
-		fprintf(stderr, "L%i: can't pchar, value out of range", line_number);
+		fprintf(stderr, "L%i: can't pchar, value out of range\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	else
